5629c723e43c2c1b671c8bbca6d82905490ec868
Kim Minje <alswp0210@naver.com>
2019-08-05 10:28:30 +0200

    2019. 08. 05 Commit

--- src/test/java/musinsa.java
+++ src/test/java/musinsa.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
-public class tous extends initialize {
+public class musinsa extends initialize {
 
     @Test
     public void test() {
--- src/test/java/testng.java
+++ src/test/java/testng.java
@@ -1,5 +1,6 @@
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
@@ -12,33 +13,13 @@ public class testng extends initialize{
 
     @Test
     public void testLogin () {
-//        driver.get("https://www.naver.com/");
-//
-//        String title = driver.getTitle();
-//        System.out.println(title);
-//        Assert.assertEquals(title, "NAVER");
-//        takeScreenShotTest(driver,title);
+        webDriver.get("https://www.naver.com/");
 
-        List<WebElement> registerlink = webDriver.findElements(By.className("liOffcanvas"));
-        registerlink.get(0).click();
-        //Enter URL
-        webDriver.get("https://www.naver.com");
-        //To change to another tab
-        Set<String> tab_handles = webDriver.getWindowHandles();
-        int number_of_tabs = tab_handles.size();
-        int new_tab_index = number_of_tabs-1;
-        webDriver.switchTo().window(tab_handles.toArray()[new_tab_index].toString());
-        //Save URL from page where you are located
-        String urlsite = webDriver.getCurrentUrl();
-        //Save variables/page elements
-        WebElement nameInput = webDriver.findElement(By. name ("firstName"));
-//        WebElement nameInput = driver.findElement(By. className ("firstName"));
-//        WebElement nameInput = driver.findElement(By. id ("firstName"));
-//        WebElement nameInput = driver.findElement(By. partialLinkText ("firstName"));
-        webDriver.findElement(By. tagName ("firstName"));
-        //Save page elements with tags not included in selenium, as title etc...
-        WebElement sitePR = webDriver.findElement(By.xpath("//*[@title='titulo']"));
-        sitePR.click();
+        String title = webDriver.getTitle();
+        System.out.println(title);
+        Assert.assertEquals(title, "NAVER");
+        takeScreenShotTest(webDriver,title);
+
     }
 
     @AfterTest
